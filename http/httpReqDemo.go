9b3fe8b09f9c8982d4187274d52602bf01b46d21
xieshaobing <shaobingxie@gmail.com>
2020-01-30 11:59:49 +0800

    desc: fix check req

--- http/httpReqDemo.go
+++ http/httpReqDemo.go
@@ -18,9 +18,14 @@ func testGet(data string) (consumer int) {
 	if req, err := getHttpReq(r); err == nil {
 		fmt.Println("method", req.Method)
 		fmt.Println("url", req.URL)
+		fmt.Println("content-length", req.ContentLength)
 		if "POST" == req.Method {
-			body, _ := ioutil.ReadAll(req.Body)
+			body, err := ioutil.ReadAll(req.Body)
+			fmt.Println("err:", err)
 			fmt.Println("body:", string(body))
+			if err == nil && req.ContentLength != int64(len(body)) {
+				return -1
+			}
 		}
 		return r.Buffered()
 	}
@@ -36,7 +41,12 @@ func main() {
 	fmt.Println("get2 len", len(get2))
 	fmt.Println("get2 len consumer", testGet(get2))
 
+	fmt.Println("---------------------")
 	post := "POST /abc/ee HTTP/1.1\r\nContent-Length: 3\r\n\r\nGET"
 	fmt.Println("post len", len(post))
 	fmt.Println("post len consumer", testGet(post))
+
+	post = "POST /abc/ee HTTP/1.1\r\nContent-Length: 3\r\n\r\n"
+	fmt.Println("post2 len", len(post))
+	fmt.Println("post2 len consumer", testGet(post))
 }
