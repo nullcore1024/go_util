1f7f38a57c0bcead640b962bc2aecee48c860516
nullcore1024 <shaobingxie@gmail.com>
2020-03-14 23:10:52 +0800

    desc: add tag test

---
+++ tag/tag.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type User struct {
+	Name string `field:"username" type:"nvarchar(20)"`
+	Age  int    `field:"age" type:"tinyint"`
+}
+
+func main() {
+	var u User
+	t := reflect.TypeOf(u)
+	f, _ := t.FieldByName("Name")
+	fmt.Println(f.Tag)
+	fmt.Println(f.Tag.Get("field"))
+	fmt.Println(f.Tag.Get("type"))
+}
