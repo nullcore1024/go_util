6ebcc7a1122cb86733e87a2d36d13d7362154c35
nullcore1024 <shaobingxie@gmail.com>
2020-03-18 18:57:49 +0800

    desc: add sample

--- gracefull_server/http/http_server.go
+++ gracefull_server/http/http_server.go
@@ -6,79 +6,133 @@ import (
 	"github.com/gorilla/mux"
 	"math/rand"
 	"net/http"
-	"strconv"
+	"time"
 )
 
-type Post struct {
-	ID    string `json:"id"`
-	Title string `json:"title"`
-	Body  string `json:"body"`
+type App_rtt_rate_req struct {
+	Seqid        int64  `json:"seqid"`
+	Uid          int64  `json:"uid"`
+	Appid        int32  `json:"appid"`
+	Mnc          string `json:"mnc"`
+	Mcc          string `json:"mcc"`
+	Country_code string `json:"country_code"`
 }
 
-var posts []Post
-
-func getPosts(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(posts)
+type Sample_rate struct {
+	Uri    int32         `json:"uri"`
+	Rate   int32         `json:"rate"`
+	Expire time.Duration `json:"expire"`
 }
 
-func createPost(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var post Post
-	_ = json.NewDecoder(r.Body).Decode(&post)
-	post.ID = strconv.Itoa(rand.Intn(1000000))
-	posts = append(posts, post)
-	json.NewEncoder(w).Encode(&post)
+type App_rtt_rate_res struct {
+	Seqid                int64         `json:"seqid"`
+	Uid                  int64         `json:"uid"`
+	Rates                []Sample_rate `json:"rates"`
+	Expire_time          int32         `json:"expire_time"`
+	Report_interval_time int32         `json:"report_interval_time"`
 }
 
-func getPost(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for _, item := range posts {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
+type Sample_uri_rtt struct {
+	Uri        int32 `json:"uri"`
+	Avg_rtt_ms int32 `json:"avt_rtt_ms"`
+	Rtt_total  int32 `json:"rtt_total"`
+}
+
+type App_report_rtt_stat_req struct {
+	Seqid int64          `json:"seqid"`
+	Uid   int64          `json:"uid"`
+	Stat  Sample_uri_rtt `json:"stat"`
+}
+
+type App_report_rtt_stat_res struct {
+	Seqid    int64 `json:"seqid"`
+	Uid      int64 `json:"uid"`
+	Res_code int32 `json:"res_code"`
+}
+
+//rand.Intn(1000000)
+type Uri_report_sample_config struct {
+	UriRate              map[int32]Sample_rate
+	CountryCodeUriRate   map[string][]Sample_rate
+	Expire               time.Duration `json:"expire"`
+	Report_interval_time time.Duration `json:"report_interval"`
+	Rate                 int32
+}
+
+var SampleConfig Uri_report_sample_config
+
+func init() {
+	SampleConfig = Uri_report_sample_config{
+		UriRate:            make(map[int32]Sample_rate),
+		CountryCodeUriRate: make(map[string][]Sample_rate),
+		Rate:               50,
 	}
-	json.NewEncoder(w).Encode(&Post{})
+	max := 10
+	for i := 0; i < max; i++ {
+		sample := Sample_rate{
+			Uri:  int32(rand.Intn(50)),
+			Rate: int32(rand.Intn(100)),
+		}
+		SampleConfig.UriRate[sample.Uri] = sample
+	}
 }
 
-func updatePost(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range posts {
-		if item.ID == params["id"] {
-			posts = append(posts[:index], posts[index+1:]...)
-			var post Post
-			_ = json.NewDecoder(r.Body).Decode(&post)
-			post.ID = params["id"]
-			posts = append(posts, post)
-			json.NewEncoder(w).Encode(&post)
-			return
-		}
+func get_sample_rate_by_country(seqId, uid int64, appId int32, mnc, mcc, country_code string, res *App_rtt_rate_res) bool {
+	v, ok := SampleConfig.CountryCodeUriRate[country_code]
+	if ok {
+		res.Rates = v
+		return true
 	}
-	json.NewEncoder(w).Encode(posts)
+	return false
 }
 
-func deletePost(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range posts {
-		if item.ID == params["id"] {
-			posts = append(posts[:index], posts[index+1:]...)
-			break
-		}
+func get_sample_rate_by_uid(seqId, uid int64, appId int32, mnc, mcc, country_code string, res *App_rtt_rate_res) bool {
+	rate := rand.Intn(100)
+	if int32(rate) < SampleConfig.Rate {
+		return false
 	}
-	json.NewEncoder(w).Encode(posts)
+	if hit := get_sample_rate_by_country(seqId, uid, appId, mnc, mcc, country_code, res); hit {
+		return true
+	}
+	for _, v := range SampleConfig.UriRate {
+		res.Rates = append(res.Rates, v)
+	}
+	return true
+}
+
+func pull_rtt_rate(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var req App_rtt_rate_req
+	_ = json.NewDecoder(r.Body).Decode(&req)
+
+	res := App_rtt_rate_res{
+		Seqid: req.Seqid,
+		Uid:   req.Uid,
+	}
+	get_sample_rate_by_uid(req.Seqid, req.Uid, req.Appid, req.Mnc, req.Mcc, req.Country_code, &res)
+	json.NewEncoder(w).Encode(&res)
+}
+
+func save_sample_rtt_stat(req *App_report_rtt_stat_req) error {
+	return nil
+}
+
+func report_uri_stat(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var req App_report_rtt_stat_req
+	_ = json.NewDecoder(r.Body).Decode(&req)
+	save_sample_rtt_stat(&req)
+	res := App_report_rtt_stat_res{
+		Seqid:    req.Seqid,
+		Uid:      req.Uid,
+		Res_code: 200,
+	}
+	json.NewEncoder(w).Encode(&res)
 }
 
 func main() {
 	router := mux.NewRouter()
-	posts = append(posts, Post{ID: "1", Title: "My first post", Body: "This is the content of my first post"})
-	router.HandleFunc("/posts", getPosts).Methods("GET")
-	router.HandleFunc("/posts", createPost).Methods("POST")
-	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
-	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
-	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
+	router.HandleFunc("/pull_rtt_rate", pull_rtt_rate).Methods("POST")
+	router.HandleFunc("/report_uri_stat", report_uri_stat).Methods("POST")
 	endless.ListenAndServe("localhost:8900", router)
 }
